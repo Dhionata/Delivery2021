82161f89bb230ce94799ca42d6e2e12ebc843391
Dhionatã <xiyunagi@gmail.com>
2021-09-01 20:35:16 -0300

    commit teste

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -28,7 +28,7 @@ public class DemoApplication {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
 		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://localhost:5432/crud");
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
 		dataSource.setUsername("postgres");
 		dataSource.setPassword("postgres");
 
