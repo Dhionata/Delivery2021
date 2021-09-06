a6ada00f145e6eb40fb249fe3e02a092b9377b45
Dhionatã <xiyunagi@gmail.com>
2021-09-06 12:39:20 -0300

    import desnecessário.

--- src/main/java/com/entities/interfaces/AvaliacaoInterface.java
+++ src/main/java/com/entities/interfaces/AvaliacaoInterface.java
@@ -1,7 +1,5 @@
 package com.entities.interfaces;
 
-import com.entities.classes.Fornecedor;
-
 public interface AvaliacaoInterface {
    // int nota = 0;
     //Fornecedor fornecedor = new Fornecedor();
