3ee6d193cfac9e34a29c09bd10edae9cdbeb0219
Dhionatã <xiyunagi@gmail.com>
2021-10-02 18:02:40 -0300

    retirada propriedade "mensagem" e adicionada "notNull", junto com revirifação do método "validate" para verificar se entidade é ou não nula.

--- src/main/java/br/com/Services/GenericService.java
+++ src/main/java/br/com/Services/GenericService.java
@@ -1,7 +1,5 @@
 package br.com.Services;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
@@ -10,84 +8,64 @@ import org.springframework.stereotype.Service;
 @Service
 public abstract class GenericService<T, R> {
     private CrudRepository<T, Integer> repository;
-    private String mensagem;
+    private final String notNull = "A entidade não pode ser nulla!";
 
     public GenericService(CrudRepository<T, Integer> repository) {
         setRepository(repository);
     }
 
-    public List<T> sendObjects(List<T> list) {
-        return list;
-    }
-
-    public Object sendErrorMessage(String mensagem) {
-        setMensagem(mensagem);
-        System.out.println(getMensagem());
-        return getMensagem();
-    }
-
     public Object findAll() {
-        try {
-            return getRepository().findAll();
-        } catch (Exception e) {
-            return sendErrorMessage("Erro na busca:\n" + e.getMessage());
-        }
+        return getRepository().findAll();
     }
 
     public T findById(Integer id) {
-        try {
-            return getRepository().findById(id).get();
-        } catch (Exception e) {
-            sendErrorMessage("Erro na busca por código:\n" + e.getMessage());
-            return null;
-        }
+        return getRepository().findById(id).get();
     }
 
     // Caso tenha regra de negócio ao salvar ou atualizar: sobrescrever este método
-    public void validate(T entity) throws Exception {
+    public boolean validate(T entity) throws Exception {
+        if (entity == null) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public Object save(T entity) {
         try {
-            validate(entity);
+            if (validate(entity) == true) {
 
-            T created = getRepository().save(entity);
+                getRepository().save(entity);
 
-            setMensagem("Persistência feita com sucesso!");
-
-            List<Object> objects = new ArrayList<>();
-            objects.add(created);
-            objects.add(getMensagem());
-            return objects;
+                return "Persistência feita com sucesso!" + entity;
+            } else {
+                return notNull;
+            }
         } catch (Exception e) {
-            return sendErrorMessage("Erro na persistência:\n" + e.getMessage());
+            return "Erro na persistência:\n" + e.getMessage();
         }
     }
 
     public Object update(T entity) {
         try {
-            validate(entity);
-
-            T created = getRepository().save(entity);
-
-            setMensagem("Atualização feita com sucesso!");
-
-            List<Object> objects = new ArrayList<>();
-            objects.add(created);
-            objects.add(getMensagem());
-            return objects;
+            if (validate(entity) == true) {
+                ;
+                getRepository().save(entity);
+                return "Atualização feita com sucesso!" + entity;
+            } else {
+                return notNull;
+            }
         } catch (Exception e) {
-            return sendErrorMessage("Erro na atualização:\n" + e.getMessage());
+            return "Erro na atualização:\n" + e.getMessage();
         }
     }
 
     public Object remove(Integer id) {
         try {
             getRepository().deleteById(id);
-            setMensagem("Remoção feita com sucesso!");
-            return getMensagem();
+            return "Remoção feita com sucesso!";
         } catch (Exception e) {
-            return sendErrorMessage("Erro na remoção:\n" + e.getMessage());
+            return "Erro na remoção:\n" + e.getMessage();
         }
     }
 
@@ -103,13 +81,5 @@ public abstract class GenericService<T, R> {
 
     private void setRepository(CrudRepository<T, Integer> repository) {
         this.repository = repository;
-    }
-
-    private String getMensagem() {
-        return mensagem;
-    }
-
-    private void setMensagem(String mensagem) {
-        this.mensagem = mensagem;
     }
 }
