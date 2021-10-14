57add1354b1fb4e1a19f03fd2ef91e4e51633923
Dhionatã <xiyunagi@gmail.com>
2021-10-13 21:30:56 -0300

    Retirado o comentário da antiga classe "FormaPagamento".

--- src/main/java/br/com/model/entities/classes/FormaPagamento.java
+++ src/main/java/br/com/model/entities/classes/FormaPagamento.java
@@ -19,39 +19,3 @@ public enum FormaPagamento {
         }
     }
 }
-// import javax.persistence.Entity;
-
-/*
- * import javax.persistence.Entity; import javax.persistence.GeneratedValue;
- * import javax.persistence.Id;
- * 
- * import br.com.model.entities.interfaces.FormaPagamentoInterface;
- * 
- * @Entity public class FormaPagamento implements FormaPagamentoInterface {
- * 
- * @Id
- * 
- * @GeneratedValue private Integer id;
- * 
- * private String nomeForma;
- * 
- * public FormaPagamento(String nomeForma) { setNomeForma(nomeForma); }
- * 
- * public FormaPagamento() { }
- * 
- * @Override public String toString() { return "\n-- FormaPagamento --\nID: " +
- * getId() + "\nNome da forma de pagamento: " + getNomeForma(); }
- * 
- * @Override public String formaDePagamento() { // TODO Auto-generated method
- * stub return null; }
- * 
- * // Getters / Setters
- * 
- * public Integer getId() { return id; }
- * 
- * public void setId(Integer id) { this.id = id; }
- * 
- * public String getNomeForma() { return nomeForma; }
- * 
- * public void setNomeForma(String nomeForma) { this.nomeForma = nomeForma; }
- */
